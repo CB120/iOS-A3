4bbddde6442d2c41a4a8e33bd991568ae32f2b80
b1cboi <76939198+b1cboi@users.noreply.github.com>
2022-05-17 23:01:47 +1000

    Added date picker to homeview

--- PillPopr/PillPopr/Controllers/MedicationScheduleController.swift
+++ PillPopr/PillPopr/Controllers/MedicationScheduleController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class MedicationScheduleController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        dateTextField.text = formatter.string(from: date)
+        dateTextField.textColor = .blue
+        
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged), for: UIControl.Event.valueChanged)
+        datePicker.frame.size = CGSize(width: 0, height: 250)
+        dateTextField.inputView = datePicker
+        
+        
     }
-
+    @objc func datePickerValueChanged(sender: UIDatePicker){
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        dateTextField.text = formatter.string(from: sender.date)
+    }
+    
 
 }
 
