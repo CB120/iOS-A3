ea2c2b661ef6a3e12b11395d0b00ebf60b77bef9
simon <runbox13@hotmail.com>
2022-05-19 22:10:35 +1000

    test demo

--- PillPopr/PillPopr/Controllers/HomeController.swift
+++ PillPopr/PillPopr/Controllers/HomeController.swift
@@ -9,19 +9,34 @@
 import UIKit
 //var newMedicationScheduleController = MedicationScheduleController();
 
-class HomeController: UIViewController {
+class HomeController: UIViewController, UITableViewDataSource {
+
+    
 
     @IBOutlet weak var scheduleDate: UILabel!
     @IBOutlet weak var welcomeTitle: UILabel!
     @IBOutlet weak var userScheduleDate: UILabel!
+    @IBOutlet weak var table:UITableView!
+    
+    struct node{
+        let title: String
+        let click: Bool
+    }
+    
+    let data:[node] = [
+        node(title: "panadol", click: false),
+        node(title: "vitamin D", click: false),
+        node(title: "omega 3", click: false),
+        node(title: "vitamin C", click: false)
+    ]
     
     var name: String = "User";
     var scheduleVar: String = "";
 
     override func viewDidLoad() {
         super.viewDidLoad();
+        table.dataSource = self
         welcomeTitle.text = "Welcome " + name;
-        userScheduleDate.text = scheduleVar;
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -30,4 +45,17 @@ class HomeController: UIViewController {
             VC.scheduleDate = scheduleVar;
         }
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let node = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.label.text = node.title
+        cell.clickme.isOn = node.click
+        return cell
+    }
+    
 }
