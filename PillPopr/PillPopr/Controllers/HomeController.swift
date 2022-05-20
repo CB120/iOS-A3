5232caf2b0beb912df3f15324d7f866c475a0381
simon <runbox13@hotmail.com>
2022-05-20 19:23:03 +1000

    crud tableview

--- PillPopr/PillPopr/Controllers/HomeController.swift
+++ PillPopr/PillPopr/Controllers/HomeController.swift
@@ -9,6 +9,11 @@
 import UIKit
 //var newMedicationScheduleController = MedicationScheduleController();
 
+struct idk: Codable{
+    let title: String
+    let click: Bool
+}
+
 class HomeController: UIViewController, UITableViewDataSource {
 
     
@@ -18,23 +23,24 @@ class HomeController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var userScheduleDate: UILabel!
     @IBOutlet weak var table:UITableView!
     
-    struct node{
-        let title: String
-        let click: Bool
-    }
+   
     
-    let data:[node] = [
-        node(title: "panadol", click: false),
-        node(title: "vitamin D", click: false),
-        node(title: "omega 3", click: false),
-        node(title: "vitamin C", click: false)
-    ]
+    let KEY_HIGH_SCORE = "highScore"
+    
+    var medlist:[idk] = [
+        idk(title: "panadol", click: false),
+        idk(title: "vitamin D", click: false),
+        idk(title: "omega 3", click: false),
+        idk(title: "vitamin C", click: false)
+]
+
     
     var name: String = "User";
     var scheduleVar: String = "";
 
     override func viewDidLoad() {
         super.viewDidLoad();
+        self.medlist = readHighScore()
         table.dataSource = self
         welcomeTitle.text = "Welcome " + name;
     }
@@ -46,16 +52,61 @@ class HomeController: UIViewController, UITableViewDataSource {
         }
     }
     
+    func writeHighScore(tempMed: idk){
+        let defaults = UserDefaults.standard
+        
+        //read from UserDefaults
+        medlist = readHighScore()
+        
+        //add data from game
+        medlist.append(tempMed)
+        
+        //adds to UserDefaults
+        defaults.set(try? PropertyListEncoder().encode(medlist), forKey: KEY_HIGH_SCORE)
+        
+    }
+    
+    func readHighScore() -> [idk]{
+        let defaults = UserDefaults.standard
+        //return highscore array from UserDefaults
+        if let savedArraryData = defaults.value(forKey: KEY_HIGH_SCORE) as? Data{
+            if let array = try? PropertyListDecoder().decode(Array<idk>.self, from: savedArraryData){
+                return array
+                
+            }else{
+                 return []
+            }
+            }
+            else{
+                return []
+                
+            }
+
+    }
+
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return medlist.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let node = data[indexPath.row]
+        
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        let node = medlist[indexPath.row]
         cell.label.text = node.title
         cell.clickme.isOn = node.click
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            medlist.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            let defaults = UserDefaults.standard
+            defaults.set(try? PropertyListEncoder().encode(medlist), forKey: KEY_HIGH_SCORE)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
     }
     
 }
--- PillPopr/PillPopr/Controllers/NewMedController.swift
+++ PillPopr/PillPopr/Controllers/NewMedController.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class NewMedController: UIViewController {
-
+    
+    @IBOutlet weak var medName: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+        // Do any additional setup after loading the view.
+    
+    
+    @IBAction func bubblePressed(_ sender: UIButton){
+        print("working")
+        let asd = HomeController()
+        asd.writeHighScore(tempMed: idk(title:medName.text!, click:false))
+    }
+
 
 
 }
